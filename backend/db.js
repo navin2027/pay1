f9fc8b6bf0548ea4c46d64302490558d3b038700
Navin Kumar <yesnavinkumar33@gmail.com>
2025-07-14 19:06:22 +0530

    Update db.js
    url hided

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://navinkumar:Navin%40cohort@navin-kumar-m.jrhujsr.mongodb.net/paytm");
+mongoose.connect("database-url");
 
 const userSchema = new mongoose.Schema({
     username : {
